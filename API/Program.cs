fb608ade17151bef9fd1328e93f23df681a8a76a
nvshah <nipunshah6776@gmail.com>
2023-11-09 23:56:28 +0530

    Add DB create or update setup before app run

--- API/Program.cs
+++ API/Program.cs
@@ -3,8 +3,9 @@ using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. (ie DI)
 
+// All these services under builder are scoped to it (ie Used for incoming HTTP requests)
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -28,5 +29,32 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+/*
+GOAL -> Make Ready DB before App Runs (ie app.Run()) ---
+Code simulating `dotnet ef database update` to create database if not
+-
+-> For this we need Service (DataContext), 
+-> as we dont have DI accessible in Program.cs, we will create scope for services under app instance (ie context)
+
+NOTE: Service of builder is diff instance than Service for an app as both have diff scope
+*/
+
+// Create Scope so as to access required Service (& destroy it after done)
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    // Try creating DB (corresp to migration)
+    var context = services.GetRequiredService<DataContext>();
+    // Effectively below is doing same as command `dotnet ef database update`
+    context.Database.Migrate();
+}
+catch (Exception ex)
+{   
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migrations");
+}
 
 app.Run();
