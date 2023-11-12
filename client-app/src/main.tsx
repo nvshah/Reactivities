b6737ae6ecf7cbe0bd8d3e044c15b6ab52d57fa2
nvshah <nipunshah6776@gmail.com>
2023-11-12 15:56:56 +0530

    client app folder structure - remove unused files

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './app/App.tsx'
 import 'semantic-ui-css/semantic.min.css'
-import './index.css'
+import './app/styles.css'
+
+// main.tsx (created via VIte) = index.tsx (if created via react-app)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
