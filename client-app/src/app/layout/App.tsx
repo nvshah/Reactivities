beb84e21e8802baad2d8077d9c9288abb3b922fa
nvshah <nipunshah6776@gmail.com>
2023-11-16 22:23:29 +0530

    client-app: delete activity impl

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -60,22 +60,28 @@ function App() {
     setSubmitting(true);
     if(activity.id){
       agent.Activities.update(activity).then(() => {
-        setActivities([...activities.filter(x => x.id !== activity.id), activity])
+        setActivities([...activities.filter(x => x.id !== activity.id), activity]); 
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
       });
     }else{
       activity.id = uuid()
       agent.Activities.create(activity).then(() => {
         setActivities([...activities, activity ]); 
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
       });
-    }
-
-    setSelectedActivity(activity);
-    setEditMode(false);
-    setSubmitting(false);
+    }    
   }
 
   function handleDeleteActivity(id: string){
-    setActivities([...activities.filter(x => x.id !== id)]);
+    setSubmitting(true);
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter(x => x.id !== id)]);
+      setSubmitting(false);
+    })
   }
 
   if (loading) return <LoadingComponent content='Loading app...' />
