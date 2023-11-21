48dc49d0f40bbb859fe1c38166e0f47dd33e00de
nvshah <nipunshah6776@gmail.com>
2023-11-22 00:29:00 +0530

    clinet-app: add activities load action - resolve STRICT-MODE warning

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,7 +1,38 @@
+import { makeAutoObservable, runInAction } from "mobx"
+import { Activity } from "../models/activity"
+import agent from "../api/agent";
+
 export default class ActivityStore{
-    title = "Something"
+    activities: Activity[] = [];
+    selectedActivity: Activity | null = null;
+    editMode = false;
+    loading = false;
+    //loadingInitial = false;
+    //submitting = false;
 
     constructor(){
+        // i auto identify which props to observe & what action is corresponding to it
+        makeAutoObservable(this)  // auto observe & action bounding
+    }
+
+    loadActivities = async () => {
+        this.loading = true 
+        try {
+            const response = await agent.Activities.list()
+            runInAction(() => { // update to state(observable) must in action (STRICT-MODE-MOBX)
+                response.forEach((a) => {
+                    a.date = a.date.split('T')[0];
+                    this.activities.push(a)
+                })
+                this.loading = false 
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false 
+            })
+        }
+
         
     }
 }
